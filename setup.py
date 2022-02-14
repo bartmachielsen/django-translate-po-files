0075c23491809c046d6ae934f55640ad8159c0c8
Bart Machielsen <bartmachielsen@gmail.com>
2022-02-14 15:52:18 +0100

    Initial commit, basic configuration of auto-django-translate

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='django-translate-po-files',
+    version='0.0.1',
+    scripts=['django-translate-po'],
+    author="Bart Machielsen",
+    author_email="bartmachielsen@gmail.com",
+    description="Automatically translate all Django PO files in the correct languages using google translate.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bartmachielsen/django-translate-po-files",
+    packages=setuptools.find_packages(),
+    requires=[
+        "polib",
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
