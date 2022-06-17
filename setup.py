313e86f0986996bf729e8d680d79e578381738ff
Bart Machielsen <bartmachielsen@gmail.com>
2022-06-17 11:32:21 +0200

    0.0.8 - po files translation.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='django-translate-po-files',
-    version='0.0.7',
+    version='0.0.8',
     scripts=['django-translate-po'],
     author="Bart Machielsen",
     author_email="bartmachielsen@gmail.com",
